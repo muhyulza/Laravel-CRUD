1ff85dca1587b5c1bda95af5908df3d968b1bc6c
unknown <vriday18@gmail.com>
2019-05-09 08:36:26 +0700

    input CRUD

---
+++ database/migrations/2019_05_09_004629_create_books_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBooksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('Judul');
+            $table->string('Penerbit');
+            $table->string('Tahun_Terbit');
+            $table->string('Pengarang');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('books');
+    }
+}
